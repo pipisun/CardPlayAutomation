5ff7cec7cc35af8d47ab0e59a9acf1aae98df9f1
Jeremy <sst1978@163.com>
2019-03-03 15:51:07 -0600

    Added real device testing.

--- tests/card/NewCardRealDeviceTest.java
+++ tests/card/NewCardRealDeviceTest.java
@@ -10,23 +10,26 @@ import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class NewCardTest {
+public class NewCardRealDeviceTest {
 
     public static final String USERNAME = "pipisun";
     public static final String ACCESS_KEY = "0e2855d5-ce41-4fa2-988f-581d4138d5f6";
-    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+//    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@us1.appium.testobject.com/wd/hub";
+    public static final String URL = "https://us1.appium.testobject.com/wd/hub";
     AndroidDriver driver;
 
     @Before
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("testobject_api_key", "2B637A2B5246433D8DE05D77063B20E2");
+//        capabilities.setCapability("testobject_app_id", "#1");
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability("platformVersion", "4.4");
-        capabilities.setCapability("app", "sauce-storage:sc.apk");
-        capabilities.setCapability("browserName", "");
-        capabilities.setCapability("deviceOrientation", "portrait");
-//        capabilities.setCapability("appiumVersion", "1.10.0");
+        capabilities.setCapability("deviceName", "Samsung_Galaxy_S6_sjc_free");
+        capabilities.setCapability("platformVersion", "7.0");
+//        capabilities.setCapability("phoneOnly", "false");
+//        capabilities.setCapability("tabletOnly", "false");
+//        capabilities.setCapability("privateDevicesOnly", "false");
+        capabilities.setCapability("appiumVersion", "1.10.1");
 
         driver = new AndroidDriver<>(new URL(URL), capabilities);
 
